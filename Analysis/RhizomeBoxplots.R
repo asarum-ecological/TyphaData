638a9556873e4f8fef8bae4cf325b9474349d158
Daniel <daniel.stewart@ubc.ca>
2019-10-09 10:01:38 -0700

    Introduced new plot formats

--- Analysis/RhizomeBoxplots.R
+++ Analysis/RhizomeBoxplots.R
@@ -7,12 +7,30 @@ library("car")
 
 #Linking the Dataset
 RhizomeData <- read.csv("~/Git/TyphaData/FieldData/2019_RhizomeData.csv")
-
 #Create boxplot
-RhizomeData %>% 
+RhizomeBoxplot <- RhizomeData %>% 
   ggplot(aes(TREATMENT, WEIGHT)) +
   geom_boxplot() +
   theme_bw() +
-  ylab("WEIGHT (g)")+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+  ylab("Weight (g)")+
   xlab ("")
-  
+RhizomeBoxplot
+
+#Ridge Plot Creation
+RhizomeRidge <- ggplot(data = RhizomeData, aes(WEIGHT, TREATMENT)) + #note that switching the axis values fixes the y-value issue with ridgeplot
+  ggridges::geom_density_ridges() +
+  labs(x = "Weight (g)", y = "") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+RhizomeRidge
+
+#Jitter Plot Creation
+RhizomeJitter <- RhizomeData %>% 
+  ggplot(aes(TREATMENT, WEIGHT)) +
+  geom_jitter(aes(colour = TREATMENT)) +
+  theme_bw() + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + 
+  ylab("Weight (g)")+
+  xlab ("")
+RhizomeJitter
--- Analysis/RichnessComparisons.R
+++ Analysis/RichnessComparisons.R
@@ -57,19 +57,32 @@ FrenchiesOutNativeRichness$LOCATION  <- "Frenchies Outside"
 Combined <- rbind(ReferenceNativeRichness,FrenchiesOutNativeRichness,FrenchiesINNativeRichness)
 
 RichnessBox <- ggplot(data = Combined, aes(x=LOCATION, y=n))+  
-  geom_density_ridges () +
+  geom_boxplot() +
   theme_bw() +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
-  labs(x = "Location", y = "Species Richness") 
+  labs(x = "", y = "Species Richness") 
 
-RichnessBox <-  boxplot(Combined$n, Combined$LOCATION)
 RichnessBox
 
+#Ridge Plot Creation
+RichnessRidge <- ggplot(data = Combined, aes(n, LOCATION)) + #note that switching the axis values fixes the y-value issue with ridgeplot
+  ggridges::geom_density_ridges() +
+  labs(x = "Species Richness", y = "") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+  RichnessRidge
 
-
+#Jitter Plot Creation
+RichnessJitter <- ggplot(data = Combined, aes(LOCATION, n)) +
+    geom_jitter(aes(colour = LOCATION)) +
+    theme_bw() +
+    ylab("Species Richness")+
+    xlab ("") + 
+    theme(legend.title = element_blank()) +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) 
+  RichnessJitter
+  
+  
 library(reshape2)
 BoxplotDF <- melt(Combined, id.var="LOCATION")
-
-
-ReferenceNativeRichness$n <- "Reference"
 
