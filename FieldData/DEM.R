daeaf8718b8930778fb1cd89f10efd899b8300d2
Daniel <daniel.stewart@ubc.ca>
2020-03-10 14:51:36 -0700

    EOD March 10

--- FieldData/DEM.R
+++ FieldData/DEM.R
@@ -3,17 +3,19 @@ library("readxl")
 library("dplyr")
 library("data.table")
 
-ElevationData <- read.csv("~/Git/TyphaData/DEM/ElevationData.csv")
+ElevationData <- read.csv("~/Git/TyphaData/DEM/TYGL.csv")
 
 Boxplot <- ElevationData %>%
-  ggplot(aes(y=VALUE)) +
+  ggplot(aes(y=RASTERVALUE)) +
            geom_boxplot() +
            theme_bw() +
            theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
          
   summary(ElevationData)
-hist(round(ElevationData$VALUE,1))
+  
+  
+hist(round(ElevationData$RASTERVALU,1))
          
   
   ggplot(ElevationData) +
-  geom_boxplot(aes(y = "VALUE"))
+  geom_boxplot(aes(y = "RASTERVALU"))
